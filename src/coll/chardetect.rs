8f1fa61612bc3d553769e775a24abc1ffc0361ce
biluohc <biluohc@qq.com>
2017-09-19 16:32:34 +0800

    empty charset meaning binary.

--- src/coll/chardetect.rs
+++ src/coll/chardetect.rs
@@ -20,8 +20,11 @@ impl CharDet {
          .for_each(|file| 
          match chardet(&file) {
              Ok(o)=> {
-                 let (charset, rate, info) = o ; 
+                 let (mut charset, rate, info) = o ; 
                  // "WINDOWS_1258".len() = 12 -> 12+6 = 18
+                 if charset.is_empty() {
+                     charset="Binary".to_owned();
+                 }
                  println!("{}: {}  {:.4}{:6}{}", space_fix(file,max_len), space_fix(&charset, 18), rate, "", info);
              }
              Err(e)=> {
@@ -47,14 +50,6 @@ impl CharDet {
 fn chardet(f :&str)->io::Result<(String, f32, String)> {
     let mut file = File::open(f)?;
     let mut bytes = Vec::default();
-    let mut buf = [0u8];
-    loop {
-        let len = file.read(&mut buf)?;
-        if len != 0 {
-            bytes.push(buf[0]);
-        } else {
-            break;
-        }
-    }
+    file.read_to_end(&mut bytes)?;
     Ok(detect(bytes.as_slice()))
 }
