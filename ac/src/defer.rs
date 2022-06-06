ed18782faeea4b0ec9d3eef027c7bfd93b4c2f7a
Wspsxing <biluohc@qq.com>
2022-06-06 15:33:39 +0800

    allow defer return other value

--- ac/src/defer.rs
+++ ac/src/defer.rs
@@ -1,29 +1,29 @@
-pub struct TakedDefer<F: FnOnce()>(Option<F>);
+pub struct TakedDefer<O, F: FnOnce() -> O>(Option<F>);
 pub trait Take {
     type T;
     fn take(&mut self) -> Option<Self::T>;
 }
 
-impl<T: FnOnce()> Take for TakedDefer<T> {
+impl<O, T: FnOnce() -> O> Take for TakedDefer<O, T> {
     type T = T;
     fn take(&mut self) -> Option<Self::T> {
         self.0.take()
     }
 }
 
-impl<F: FnOnce()> Drop for TakedDefer<F> {
+impl<O, F: FnOnce() -> O> Drop for TakedDefer<O, F> {
     fn drop(&mut self) {
         self.take().map(|f| f());
     }
 }
 
 /// defer execute the closure until the drop
-pub fn defer<F: FnOnce()>(f: F) -> impl Drop + Take<T = F> {
+pub fn defer<O, F: FnOnce() -> O>(f: F) -> impl Drop + Take<T = F> {
     TakedDefer(Some(f))
 }
 
 #[test]
-fn test() {
+fn test_defer() {
     use std::cell::RefCell;
 
     let i = RefCell::new(0);
@@ -40,4 +40,26 @@ fn test() {
         _d.take();
     }
     assert_eq!(*i.borrow(), 1);
+}
+
+#[test]
+fn test_defer_output() {
+    use std::sync::atomic::{AtomicU8, Ordering};
+
+    let x = AtomicU8::new(0);
+    let l = || x.load(Ordering::Acquire);
+    let i = || x.fetch_add(1, Ordering::Release);
+
+    assert_eq!(l(), 0);
+    {
+        let _d = defer(|| i());
+        assert_eq!(l(), 0);
+    }
+    assert_eq!(l(), 1);
+
+    {
+        let mut _d = defer(i);
+        (_d.take().unwrap())();
+    }
+    assert_eq!(l(), 2);
 }
